a135f64872b5668329caa4126182193884e4226d
konradudohannes <konrad.woelms@gmail.com>
2021-02-24 23:27:22 +0100

    replaced old blog name in all files

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/open-data-ideas/assets/js/search-data.json', true);
+    request.open('GET', '/correlaid-codes/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
